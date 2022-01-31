1f738a47a21fd096d665b9e9798aca37402f5e4c
victor-git-cairo <victor.w.cairo@gmail.com>
2022-01-30 19:45:49 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'RFormApp';
+  genders = ['male', 'female'];
+  signUpForm!: FormGroup;
+
+  // https://mailtrap.io/blog/angular-email-validation/
+  ngOnInit() {
+    this.signUpForm = new FormGroup({
+      'username': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+      'gender': new FormControl('male')
+    })
+  }
+
+  onSubmit() {
+    console.log(this.signUpForm);
+  }
 }
